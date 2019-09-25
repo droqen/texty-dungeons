6d009fbe3f61389c88bce4425755b33efb9a4400
droqen <hi@droqen.com>
2019-09-25 10:04:19 -0400

    Initial commit

---
+++ Assets/TextPrintAnimationEditVisHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextPrintAnimation))]
+[RequireComponent(typeof(TextMeshPro))]
+
+[ExecuteInEditMode]
+public class TextPrintAnimationEditVisHelper : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Application.isPlaying) { enabled = false; return; }
+        GetComponent<TextMeshPro>().text = GetComponent<TextPrintAnimation>().text;
+    }
+}
